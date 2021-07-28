7153f3461b451f0bab392e8d2db26a263bbb72b8
Syed Jafri <syed.jafri2@ucalgary.ca>
2021-07-28 02:46:15 -0600

    Add typing:

---
+++ src/Camera.d.ts
@@ -0,0 +1,7 @@
+import * as ios from './Camera.ios';
+import * as android from './Camera.android';
+
+declare var _test: typeof ios;
+declare var _test: typeof android;
+
+export * from './Camera.ios';
