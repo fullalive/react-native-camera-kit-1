9c7506ce4370b730a78795d1a322e382eee5799f
Syed Jafri <syed.jafri2@ucalgary.ca>
2021-07-28 02:55:26 -0600

    update typing

--- src/Camera.d.ts
+++ src/Camera.d.ts
@@ -1,7 +1,1 @@
-import * as ios from './Camera.ios';
-import * as android from './Camera.android';
-
-declare var _test: typeof ios;
-declare var _test: typeof android;
-
-export * from './Camera.ios';
+export const Camera: React.ForwardRefExoticComponent<React.RefAttributes<unknown>>;
